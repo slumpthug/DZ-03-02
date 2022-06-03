c27c76190786a6705af7129a54411b3e530735e4
slumpthug <gangguchi58@gmail.com>
2022-06-03 22:25:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+//Задание 1
+// let a = 9;
+
+// if (a == 10) {
+//     console.log("Верно");
+// } else {
+//     console.log("Не верно");
+// }
+
+//Задание 2
+// let a = -1;
+
+// if (a >= 0) {
+//     console.log("Верно");
+// } else {
+//     console.log("Не верно");
+// }
+
+//Задание 3
+// let test = false;
+
+// if (test == true) {
+//     console.log("Верно");
+// } else {
+//     console.log("Не верно");
+// }
+
+//Короткая запись
+// test == true ? console.log('Верно') : console.log('Не верно');
+
+//Задание 4
+// let a = 1;
+// let b = 5;
+
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+//     console.log("Верно");
+// } else {
+//     console.log("Не верно");
+// }
+
+//Задание 5
+// const num = 1;
+
+// switch (num); {
+//     case 1:
+//         console.log('Зима');
+//         break;
+//     case 2:
+//         console.log('Весна');
+//         break;
+//     case 3:
+//         console.log('Лето');
+//         break;
+//     case 4:
+//         console.log('Осень');
+//         break;
+//     default:
+//         console.log('Ничего');
+//         break;
+// }
+
+// const result = num;
+// console.log(result);
+
+//Доп. задание 1
+// let x = 10;
+// let y = 7;
+
+// x > y ? console.log('x больше, чем y') : console.log('x не больше, чем y');
+
+//Доп. задание 2
+// let month = 4;
+
+// if (month <= 3) {
+//     console.log("Зима");
+// } else if (month <= 6) {
+//     console.log("Весна");
+// } else if (month <= 9) {
+//     console.log("Лето");
+// } else if (month <= 12) {
+//     console.log("Осень");
+// } else {
+//     console.log("В одном году только 12 месяцев");
+// }
+
+//Доп. задание 3
+// let red = "нет";
+// let yellow = "нет";
+
+// if (red == "нет" && yellow == "нет") {
+//     console.log("можно переходить дорогу");
+// } else if (red == "да") {
+//     console.log("нельзя переходить дорогу");
+// } else if (yellow == "да") {
+//     console.log("вам следует подождать");
+// }
